ad52561fed6634253e059e9b554400cdb91cb040
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2024-05-03 21:28:57 +0300

    Implemented aiming formula.

--- include/CombatProjectileAimController.h
+++ include/CombatProjectileAimController.h
@@ -29,24 +29,24 @@ namespace RE
 		void                 FinishLoadGame() override;                                    // 0A
 
 		// members
-		RE::Projectile* projectile;                 // 48
-		std::uint32_t   unk50;                      // 50
-		std::uint32_t   unk54;                      // 54
-		float           turningAngle;               // 58
-		RE::NiPoint3    attackerLocation;           // 5C
-		RE::NiPoint3    projectileLaunchPoint;      // 68
-		RE::NiPoint3    aimPoint;                   // 74
-		RE::NiPoint3    targetBodyPartOffset;       // 80
-		RE::NiPoint3    aimOffset;                  // 8C
-		std::uint32_t   releaseTimestamp;           // 98
-		std::uint32_t   unk9C;                      // 9C
-		float           aimVariance;                // A0
-		std::uint32_t   unkA4;                      // A4
-		std::uint64_t   unkA8;                      // A8
-		float           unkB0;                      // B0
-		RE::NiPoint3    lastKnownProjectileOffset;  // B4
-		std::uint32_t   lastUpdatedUnkB4Timestamp;  // C0
-		std::uint64_t   unkC8;                      // C8
+		RE::BGSProjectile* projectile;                 // 48
+		std::uint32_t      unk50;                      // 50
+		std::uint32_t      groundRadius;               // 54
+		float              turningAngle;               // 58
+		RE::NiPoint3       attackerLocation;           // 5C
+		RE::NiPoint3       projectileLaunchPoint;      // 68
+		RE::NiPoint3       aimPoint;                   // 74
+		RE::NiPoint3       targetBodyPartOffset;       // 80
+		RE::NiPoint3       aimOffset;                  // 8C
+		std::uint32_t      releaseTimestamp;           // 98
+		std::uint32_t      unk9C;                      // 9C
+		float              aimVariance;                // A0
+		std::uint32_t      unkA4;                      // A4
+		std::uint64_t      unkA8;                      // A8
+		float              unkB0;                      // B0
+		RE::NiPoint3       lastKnownProjectileOffset;  // B4
+		std::uint32_t      lastUpdatedUnkB4Timestamp;  // C0
+		std::uint64_t      unkC8;                      // C8
 	};
 	static_assert(sizeof(CombatProjectileAimController) == 0xD0);
 }
--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -6,6 +6,75 @@
 
 namespace NLA
 {
+	static std::default_random_engine rnd;
+	
+	namespace Calculations
+	{
+
+		static const float maxDistance = 3072; // I didn't find the actual setting that controls at which max range NPCs can shoot. But this is roughly twice the distance that I observed in-game :)
+		static const float maxTargetSize = 500; // For reference: dragon ~ 700-1000 (depending ); giant ~ 180; human ~ 90; rabbit - 30
+
+		// C = sqrt(w/d^2)/2
+		float ShotComplexity(float distance, float targetSize) {
+			return std::sqrt(targetSize) / (2 * targetSize);
+		}
+
+		// c = 1/log(skill); skill >= 10
+		float SkillFactor(float skill) {
+			return 0.8f / std::log10(std::max(skill, 10.0f));
+		}
+
+		// TODO: Figure out the formula that also takes into account shot complexity (so that at closer range even bad archers could show some accuracy.
+		// c2 = 0.4 * e^(2*c)
+		float SkillConsistency(float skillFactor) {
+			return 0.5f * std::exp(2 * std::sqrt(skillFactor)) - 1 + skillFactor;
+		}
+		 
+		// normalize distance in range [10;100]
+		// min + (distance - Dmin) * (max - min) / (Dmax - Dmin)
+		float NormalizedDistance(float distance, float maxDistance = Calculations::maxDistance) {
+			return 10 + 90 * (std::clamp(distance, 0.0f, maxDistance) / maxDistance);
+		}
+
+		// normalize distance in range [10;100]
+		// min + (size - Smin) * (max - min) / (Smax - Smin)
+		float NormalizedTargetSize(float targetSize, float maxSize = maxTargetSize) {
+			return 10 + 90 * (std::clamp(targetSize, 0.0f, maxSize) / maxSize);
+		}
+
+		// s = 0.5 * v * c
+		float Deviation(float skillFactor, float variance) {
+			return 0.5 * skillFactor * variance;
+		}
+
+		// m = v * c^2 * log(C)
+		float Mean(float skillFactor, float shotComplexity, float variance) {
+			return variance * std::pow(skillFactor, 2) * std::log10(shotComplexity);
+		}
+
+		RE::NiPoint3 RandomOffset(float skill, float distance, float targetSize, float variance) {
+			auto skillFactor = SkillFactor(skill);
+			auto skillConsistency = SkillConsistency(skillFactor);
+
+			auto normalizedDistance = NormalizedDistance(distance, maxDistance);
+			auto normalizedTargetSize = NormalizedTargetSize(targetSize, maxTargetSize);
+			auto shotComplexity = ShotComplexity(normalizedDistance, normalizedTargetSize);
+
+			auto deviation = Deviation(skillFactor, variance);
+			auto mean = Mean(skillFactor, shotComplexity, variance);
+
+			std::uniform_real_distribution  combiner;
+			std::normal_distribution<float> distrLeft(-mean, variance);
+			std::normal_distribution<float> distrRight(mean, variance);
+
+			return {
+				skillConsistency * (combiner(rnd) > 0.5 ? distrLeft(rnd) : distrRight(rnd)),
+				skillConsistency * (combiner(rnd) > 0.5 ? distrLeft(rnd) : distrRight(rnd)),
+				skillConsistency * (combiner(rnd) > 0.5 ? distrLeft(rnd) : distrRight(rnd))
+			};
+		}
+
+	};
 
 	namespace UpdateHooks
 	{
@@ -22,6 +91,33 @@ namespace NLA
 		inline void Install() {
 			const REL::Relocation<uintptr_t> update{ RELOCATION_ID(35565, 36564) };  // 5B2FF0, 5D9F50, main update
 			stl::write_thunk_call<Nullsub>(update.address() + OFFSET(0x748, 0xC26));
+		}
+	}
+
+	namespace Options
+	{
+		static float fCombatAimProjectileRandomOffset() {
+			auto settings = RE::GameSettingCollection::GetSingleton();
+			if (auto setting = settings->GetSetting("fCombatAimProjectileRandomOffset")) {
+				return setting->data.f;
+			}
+			return 16.0f;
+		}
+
+		static float fCombatRangedAimVariance() {
+			auto settings = RE::GameSettingCollection::GetSingleton();
+			if (auto setting = settings->GetSetting("fCombatRangedAimVariance")) {
+				return setting->data.f;
+			}
+			return 0.9f;
+		}
+
+		static float fCombatAimProjectileGroundMinRadius() {
+			auto settings = RE::GameSettingCollection::GetSingleton();
+			if (auto setting = settings->GetSetting("fCombatAimProjectileGroundMinRadius")) {
+				return setting->data.f;
+			}
+			return 128.0f;
 		}
 	}
 
@@ -59,6 +155,9 @@ namespace NLA
 
 			static constexpr std::array<color, 9> pale_colors = { pale_red, pale_blue, /*pale_green,*/ pale_yellow, pale_purple, pale_cyan, pale_orange, pale_magenta, pale_lime };
 
+			static color GetRandomColor(bool pale = false) {
+				return pale ? pale_colors[std::uniform_int_distribution<std::uint32_t>{0, pale_colors.size() - 1}(rnd)] : colors[std::uniform_int_distribution<std::uint32_t>{0, colors.size() - 1}(rnd)];
+			}
 			static color GetRandomColor(const RE::TESObjectREFR* obj, bool pale = false) {
 				if (obj) {
 					if (obj == RE::PlayerCharacter::GetSingleton()) {
@@ -73,7 +172,10 @@ namespace NLA
 		};
 
 		static void Mark(const RE::NiPoint3& point, const Color::color& color) {
-			DrawHandler::DrawDebugSphere(point, 5, 0.2, color);
+			DrawHandler::DrawDebugPoint(point, 0.5, color);
+		}
+		static void Mark2(const RE::NiPoint3& point, const Color::color& color) {
+			DrawHandler::DrawDebugSphere(point, 10, 0.5, color);
 		}
 
 		static void Mark(const RE::NiBound& bound, const Color::color& color) {
@@ -88,6 +190,10 @@ namespace NLA
 			Mark(point, Color::GetRandomColor(owner, pale));
 		}
 
+		static void Mark2(const RE::NiPoint3& point, const RE::TESObjectREFR* owner, bool pale = false) {
+			Mark2(point, Color::GetRandomColor(owner, pale));
+		}
+
 		static void Mark(const RE::NiBound& bound, const RE::TESObjectREFR* owner, bool pale = false) {
 			Mark(bound, Color::GetRandomColor(owner, pale));
 		}
@@ -96,64 +202,114 @@ namespace NLA
 			Mark(from, to, Color::GetRandomColor(owner, pale));
 		}
 
+		RE::NiPoint3* GetTargetPoint(RE::NiPoint3* result, RE::TESObjectREFR* target, std::uint32_t bodyPart) {
+			using func_t = decltype(&GetTargetPoint);
+			REL::Relocation<func_t> func{ RELOCATION_ID(0, 47282) };
+			return func(result, target, bodyPart);
+		}
+
 		struct CalculateAim
 		{
+			static void PickAnotherBodyPart(RE::CombatProjectileAimController* controller, RE::Actor* target) {
+				std::uniform_int_distribution<std::uint32_t>       dist(0, 4);
+				std::uniform_real_distribution<float> offsetRND(0, 1);
+
+				// Prevent randomizing when the shot is locked to aim for the ground.
+				if (Options::fCombatAimProjectileGroundMinRadius() < controller->groundRadius && controller->unkB0 == std::numeric_limits<float>::min())
+				{
+					return;
+				}
+				std::uint32_t bodyPart = dist(rnd);
+
+				if (bodyPart) {
+					RE::NiPoint3 point;
+					RE::NiPoint3 targetPoint = controller->targetBodyPartOffset + target->data.location;
+
+					GetTargetPoint(&point, target, bodyPart);
+					RE::NiPoint3 offset = point - targetPoint;
+					auto         factor = offsetRND(rnd) * Options::fCombatRangedAimVariance();
+					controller->aimOffset.x += offset.x * factor;
+					controller->aimOffset.y += offset.y * factor;
+					controller->aimOffset.z += offset.z * factor;
+				}
+			}
+
 			static void thunk(RE::CombatProjectileAimController* controller, RE::Actor* target) 
 			{		
+				DrawHandler::GetSingleton()->OnPostLoad(); // workaround to see debug rendering when coc-ing from main menu
 				logger::info("{:*^50}", "Calculating Projectile Aim");
-				float aimOffset = 0;
-				float aimVariance = 0;
 
-				if (auto aimOffsetSetting = RE::GameSettingCollection::GetSingleton()->GetSetting("fCombatAimProjectileRandomOffset")) {
-					aimOffset = aimOffsetSetting->GetFloat();
-				}
-				if (auto aimVarianceSetting = RE::GameSettingCollection::GetSingleton()->GetSetting("fCombatRangedAimVariance")) {
-					aimVariance = aimVarianceSetting->GetFloat();
-					controller->aimVariance = aimVariance;
-				}
-
-				float           width = 0;
-				RE::NiAVObject* bounds;
-
-				if (target->IsPlayerRef() && !RE::PlayerCharacter::GetSingleton()->playerFlags.isInThirdPersonMode) {
-					width = target->IsSneaking() ? 89.3 : 93.5;
-				} else if (auto bounds = target->Get3D()) {
-					width = bounds->worldBound.radius;
-					Mark(bounds->worldBound, Color::GetRandomColor(target));
-				}
-
-				auto distance = controller->attackerLocation.GetDistance(controller->aimPoint);
-
-				logger::info("fCombatAimProjectileRandomOffset: {:.4f}", aimOffset);
-				logger::info("fCombatRangedAimVariance: {:.4f}", aimVariance);
-				logger::info("{} width: {:.4f}", target->GetName(), width);
-				logger::info("\taimVariance: {:.4f}", controller->aimVariance);
-				logger::info("width/offset: {:.4f}", width / aimOffset);
-				logger::info("distance to target: {:.4f}", distance);
-
-				// TODO: Figure out the shot complexity formula and adjustable offset.
-				controller->aimVariance = width / aimOffset;
-
-				func(controller, target);
-				DrawHandler::GetSingleton()->Update(*g_deltaTime);
-				
 				RE::Actor* attacker = nullptr;
 				if (attacker = controller->combatController->attackerHandle.get().get()) {
 					logger::info("{} is shooting at {}", attacker->GetName(), target->GetName());
 				} else {
+					func(controller, target);
 					return;
 				}
 
-				
+				if (!attacker->HasKeywordByEditorID("ActorTypeNPC") && !attacker->GetRace()->HasKeywordString("ActorTypeNPC")) {
+					logger::info("{} is not an NPC that can learn", attacker->GetName());
+					func(controller, target);
+					return;
+				}
 
-				
+				// At the moment we only support bows and crossbows. (no magic)
+				if (!controller->projectile->IsArrow()) {
+					logger::info("{} is not supported", controller->projectile->GetName());
+					func(controller, target);
+					return;
+				}
 
-			
-				Mark(controller->actualAimPoint + controller->aimOffset, attacker);
-				Mark(controller->actualAimPoint + controller->aimOffset, controller->projectileLaunchPoint, attacker, true);
-				Mark(controller->projectileLaunchPoint, attacker, true);
+				// Reset aimVariance to disable original randomization logic.
+				controller->aimVariance = 0;	
+				func(controller, target);
 
-				
+				// This emulates the original randomization of changing body parts.
+				PickAnotherBodyPart(controller, target);
+
+				float aimOffset = Options::fCombatAimProjectileRandomOffset();
+				float aimVariance = Options::fCombatRangedAimVariance();
+
+				// Check real distances and dragon size to set sensible maximums.
+				float distance = controller->attackerLocation.GetDistance(controller->aimPoint);
+				float skill = attacker->GetActorValue(RE::ActorValue::kArchery);
+				float width = 0;
+
+				if (target->IsPlayerRef() && !RE::PlayerCharacter::GetSingleton()->playerFlags.isInThirdPersonMode) {
+					width = target->IsSneaking() ? 89.3 : 93.5;  // these are values for default player size in 3rd person mode. Since 1st person always returns 1 in bounds, we manually set these.
+				} else if (auto bounds = target->Get3D()) {
+					width = bounds->worldBound.radius;
+					//Mark(bounds->worldBound, Color::GetRandomColor(target));
+				}
+
+				auto offsetFractions = Calculations::RandomOffset(skill, distance, width, aimVariance);
+
+				controller->aimOffset.x = aimOffset * offsetFractions.x;
+				controller->aimOffset.y = aimOffset * offsetFractions.y;
+				controller->aimOffset.z = aimOffset * offsetFractions.z;
+
+				// This is for debug sampling.
+				for (int i = 0; i < 100; i++) {
+					auto offsetFractions = Calculations::RandomOffset(skill, distance, width, aimVariance);
+					RE::NiPoint3 offset = {
+						aimOffset * offsetFractions.x,
+						aimOffset * offsetFractions.y,
+						aimOffset * offsetFractions.z
+					};
+					Mark(controller->actualAimPoint + offset, attacker, true);
+				}
+
+				DrawHandler::GetSingleton()->Update(*g_deltaTime);
+
+			//   Mark(controller->actualAimPoint + controller->aimOffset, attacker);
+			//	Mark(controller->actualAimPoint + controller->aimOffset, controller->projectileLaunchPoint, attacker, true);
+				Mark2(controller->projectileLaunchPoint, attacker, true);
+
+				logger::info("\tfCombatAimProjectileRandomOffset: {:.4f}", aimOffset);
+				logger::info("\tfCombatRangedAimVariance: {:.4f}", aimVariance);
+				logger::info("\tWidth of {}: {:.4f} (normalized: {:.4f}", target->GetName(), width, Calculations::NormalizedTargetSize(width));
+				logger::info("\tDistance to {}: {:.4f} (normalized: {:.4f})", target->GetName(), distance, Calculations::NormalizedDistance(distance));
+				logger::info("\tSkill of {}: {:.4f}", attacker->GetName(), attacker->GetActorValue(RE::ActorValue::kArchery));
 				logger::info("\taimOffset: {}", controller->aimOffset);
 
 			}
@@ -161,6 +317,7 @@ namespace NLA
 			static inline REL::Relocation<decltype(thunk)> func;
 		};
 
+		/// Calculate and draw shot trajectory.
 		struct WeapFireAmmo
 		{
 			static RE::ProjectileHandle* thunk(RE::ProjectileHandle* projectile, RE::Projectile::LaunchData& data) {
@@ -181,16 +338,18 @@ namespace NLA
 					data.origin.z + D * zt
 				};
 
-				DrawHandler::GetSingleton()->Update(*g_deltaTime);
-				Mark(data.origin, data.shooter);
-				Mark(data.origin, target, data.shooter);
+				//DrawHandler::GetSingleton()->Update(*g_deltaTime);
+				//Mark(data.origin, data.shooter);
+				//Mark(data.origin, target, data.shooter);
 			
+				logger::info("{} Fired an arrow!", data.shooter->GetName());
 				return func(projectile, data);
 			}
 
 			static inline REL::Relocation<decltype(thunk)> func;
 		};
 
+		/// Disable random offset for arrows.
 		struct WeapFireAmmoRangomizeArrowDirection
 		{
 			static float thunk(float min, float max) {
@@ -213,24 +372,8 @@ namespace NLA
 
 
 			logger::info("Installed PerfectAim hooks");
-
-			/*auto settings = RE::GameSettingCollection::GetSingleton();
-			if (auto aimVariance = settings->GetSetting("fCombatRangedAimVariance")) {
-				aimVariance->data.f = 0.9;
-				settings->WriteSetting(aimVariance);
-				logger::info("Set fCombatRangedAimVariance to 0.9");
-			}
-
-			if (auto aimOffset = settings->GetSetting("fCombatAimProjectileRandomOffset")) {
-				aimOffset->data.f = 0;
-				settings->WriteSetting(aimOffset);
-				logger::info("Set fCombatAimProjectileRandomOffset to 16");
-			}*/
-
-			 
 		}
 	}
-	
 
 	void Install() {
 		logger::info("{:*^30}", "HOOKS");
