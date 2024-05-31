ec9089fd78fa0ab78c5ff253af710247fc9f4e25
FlayaN <hannes.feldt@gmail.com>
2024-05-31 20:02:56 +0200

    feat: add missing vr config

--- main.ps1
+++ main.ps1
@@ -1,6 +1,6 @@
 ../pack-skse-mod/pack.ps1 -CMAKE_SE_BUILD_PRESET vs2022-windows-vcpkg-se `
                           -CMAKE_AE_BUILD_PRESET vs2022-windows-vcpkg-ae `
-                          -CMAKE_VR_CONFIG_PRESET '' `
+                          -CMAKE_VR_BUILD_PRESET vs2022-windows-vcpkg-vr `
                           -FOMOD_INCLUDE_PDB $true `
                           -FOMOD_REQUIRED_INSTALLATION_DIR 'FOMOD/Required Files' `
                           -FOMOD_MOD_NAME 'NPCs Names Distributor' `
@@ -10,7 +10,7 @@
 
 ../pack-skse-mod/pack.ps1 -CMAKE_SE_BUILD_PRESET vs2022-windows-vcpkg-se-debug `
                           -CMAKE_AE_BUILD_PRESET vs2022-windows-vcpkg-ae-debug `
-                          -CMAKE_VR_CONFIG_PRESET '' `
+                          -CMAKE_VR_BUILD_PRESET vs2022-windows-vcpkg-vr-debug `
                           -CMAKE_BUILD_CONFIGURATION: Debug `
                           -FOMOD_INCLUDE_PDB $true `
                           -FOMOD_REQUIRED_INSTALLATION_DIR 'FOMOD/Required Files' `
